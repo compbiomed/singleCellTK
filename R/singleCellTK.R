adbd4ab54e818b7ed8ce45814fd934899fccf81e
David Jenkins <d.jenkins0@gmail.com>
2016-11-03 13:55:22 -0400

    Intial indata is null to allow interactive upload

--- R/singleCellTK.R
+++ R/singleCellTK.R
@@ -7,7 +7,7 @@
 #' @return The shiny app will open
 #'
 #' @export singleCellTK
-singleCellTK <- function(inputData) {
+singleCellTK <- function(inputData=NULL) {
   appDir <- system.file("shiny", package = "singleCellTK")
   options(batchqc.shinyInput = inputData)
   shiny::runApp(appDir, display.mode = "normal")
