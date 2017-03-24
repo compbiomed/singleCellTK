1e57c4f1ce4c481a53391b4165b653d2d1f8fa3b
Sebastian Carrasco <sebastian.carrasco@upch.pe>
2017-03-24 12:51:28 -0400

    Updated Dimensonality Reduction tab, contains getPCA, getTSNE and plotDimREd funcions, also creates vals and vals to store results

---
+++ R/getTSNE.R
@@ -0,0 +1,30 @@
+#' Get t-SNE components for a feature counts table
+#'   
+#' Selects the 500 most variable genes in the feature count, performs
+#' t-SNE based on them and outputs the principal components in a data frame
+#' and their variances as percentVar attribute
+#'
+#' @param count_data Matrix of expresion values
+#' 
+#' @return A reduced dimension object
+#' @export getTSNE
+#'
+getTSNE <- function(count_data){
+  ntop <- 500
+  scale_features <- TRUE
+  exprs_mat <- exprs(count_data)
+  rv <- matrixStats::rowVars(exprs_mat)
+  feature_set <-
+    order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))]
+  exprs_to_plot <- exprs_mat[feature_set,]
+  keep_feature <- (matrixStats::rowVars(exprs_to_plot) > 0.001)
+  keep_feature[is.na(keep_feature)] <- FALSE
+  exprs_to_plot <- exprs_to_plot[keep_feature, ]
+  exprs_to_plot <- t(scale(t(exprs_to_plot), scale = scale_features))
+  perplexity = floor(ncol(count_data) / 5)
+  tsne_out <- Rtsne::Rtsne(t(exprs_to_plot),perplexity = perplexity,
+                           initial_dims = max(50, ncol(count_data)))
+  tsne_out <- data.frame(tsne_out$Y[, 1:2],
+                           row.names = sampleNames(count_data))
+  return(tsne_out)
+}
