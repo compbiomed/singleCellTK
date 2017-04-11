a9d0b164a3344edb90a19ab7d19afbc5342a28ae
irisapo <shiyiyang94@gmail.com>
2017-04-10 22:12:23 -0400

    svm tuning functionality

--- R/QC_SVM.R
+++ R/QC_SVM.R
@@ -14,9 +14,12 @@
  #' @param train_label   The training label annotation specifying the high/low quality of the training dataset.
  #' @param test_set The test dataset (cells x features)
  #' @param var feature names used for predicting cell quality ## 
+ #' @param tune_para boolean values determining if doing the parameter tuning 
+ #' @param gamma_range vector of gamma paramters provided if tune_para is TRUE
+ #' @param cost_range vecor of cost patameters provided if tune_para is TRUE
  #' @example  ToDo
  #' @export
- singlecell_SVM <- function(train_set, train_label, test_set, var){
+ singlecell_SVM <- function(train_set, train_label, test_set, var, tune_para=FALSE,gamma_range,cost_range ){
    # train data frame preparation 
         ## should examine train_set is data.frame 
    train_df <- train_set[, colnames(train_set) %in% var]
@@ -41,11 +44,29 @@
       # if the set_tune = T, then also provide a data.frame of the parameters(para =),
       # and the result would be an object, which gives the parameters for the best model 
       # as well as the prediction for the test dataset
-   sfit.tune <- e1071::tune.svm(l~., data = train_df, 
-                                sampling="fix",
-                                gamma = 2^seq(-8,8,2),   # gamma should be given by users  
-                                cost = 2^seq(-8,8,2)     # cost should be given by users
-                                )
+   if(tune_para ==TRUE){
+     if(!is.null(gamma_range) & !is.null(cost_range)){  # both gamma & cost are given
+       sfit.tune <- e1071::tune.svm(l~., data = train_df, 
+                                    sampling="fix",
+                                    gamma = gamma_range,   # gamma should be given by users  
+                                    cost = cost_range      # cost should be given by users
+                                    )
+     }else if(!is.null(gamma_range) & is.null(cost_range)){ # only gamma is given 
+       sfit.tune <- e1071::tune.svm(l~., data = train_df, 
+                                    sampling="fix",
+                                    gamma = gamma_range   # gamma should be given by users  
+                                    )
+     }else if(is.null(gamma_range) & !is.null(cost_range)){
+       sfit.tune <- e1071::tune.svm(l~., data = train_df, 
+                                    sampling="fix",
+                                    cost = cost_range   # cost should be given by users  
+                                    )
+     }else{ # neither gamma or cost is given 
+       sfit.tune <- e1071::tune.svm(l~., data = train_df, 
+                                    sampling="fix"
+                                    )
+     }
+   }  # should has output for this later
    
    
    
