63a68ee8aceb756e8a9a18a4eeba62da2e8d29b4
David Jenkins <d.jenkins0@gmail.com>
2017-05-19 18:47:55 -0400

    Added rerun TSNE button. Buxfixes for MAST and indentation fixes

--- R/MAST.R
+++ R/MAST.R
@@ -16,7 +16,7 @@ MAST <- function(SCEdata,
                  FCTHRESHOLD=log2(1.5) , p.value = 0.05 ){
   
   if(is.null(condition)){
-    stop("specify the condition of interst")
+    stop("specify the condition of interest")
   }
   
   if(length(unique(pData(SCEdata)[,condition])) == 1) {
@@ -24,9 +24,9 @@ MAST <- function(SCEdata,
   }
   
   if(is.null(interest.level) & length(unique(pData(SCEdata)[, condition]))>2 ){
-    stop("have to specify a level of interest when more than 2 levels are in the condiciton")
+    stop("You must specify a level of interest when more than 2 levels are in the condition")
   }
-  
+
   # data preparation 
   nGeneOn <- colSums(counts(SCEdata))
   pdata <- pData(SCEdata)
@@ -44,7 +44,7 @@ MAST <- function(SCEdata,
     colData(SCE_new_sample)[,condition] <- as.factor(colData(SCE_new_sample)[,condition])
   }
   ##   >2 levels in the condition 
-  if(!is.null(interest.level)){
+  if(!is.null(interest.level) & length(unique(pData(SCEdata)[, condition]))>2 ){
     levels(colData(SCE_new_sample)[,condition]) <- c(levels(colData(SCE_new_sample)[,condition]), paste0("no_", interest.level))
     colData(SCE_new_sample)[,condition][colData(SCE_new_sample)[,condition] != interest.level] <- paste0("no_", interest.level)
     colData(SCE_new_sample)[,condition] <- droplevels(as.factor(colData(SCE_new_sample)[,condition]))
@@ -58,7 +58,7 @@ MAST <- function(SCEdata,
     fcHurdle <- merge(summaryDT[summaryDT$contrast==paste0(condition, "no_", interest.level) & summaryDT$component=='H', c('primerid','Pr(>Chisq)')],   # hurdel p value 
                       summaryDT[summaryDT$contrast==paste0(condition, "no_", interest.level) & summaryDT$component=='logFC', c('primerid','coef','ci.hi','ci.lo')]  # logFC coefficients
     )  
-  }else if(is.null(interest.level)){
+  }else {
     colData(SCE_new_sample)[,condition] <- droplevels(as.factor(colData(SCE_new_sample)[,condition]))
     level.cond  <- levels(colData(SCE_new_sample)[,condition])
     
--- inst/shiny/ui_08_mast.R
+++ inst/shiny/ui_08_mast.R
@@ -4,28 +4,32 @@ shiny_panel_mast <- fluidPage(
     h1("MAST"),
     fluidPage(
       fluidRow(
-        column(4,
-               wellPanel(
-                 sliderInput("FCthreshold", "Select fold change threshold", log2(1), log2(2), 0.6),
-                 h3("Filtering"),
-                 sliderInput("hurdlethresh", "Select expression threshold", 0, 1, 0.1),
-                 sliderInput("samplesize", "Select Sample Size", 0, 500, 100),
-                 selectInput("hurdlecondition","Select Condition for Hurdle Model",clusterChoice),
-                 uiOutput("hurdleconditionofinterestUI"),
-                 sliderInput("hurdlepvalue", "p-value cutoff:", 0.01, 0.2, 0.05),
-                 selectInput("selectCorrection","Correction Methods",c("FDR", "Bonferroni")),
-                 withBusyIndicatorUI(actionButton("runDEhurdle", "Run DE Using Hurdle")),
-                 h5("Download Results"),
-                 downloadButton("downloadHurdlerResult","Download Results")
-               )),
-        column(8,
-               tabsetPanel(
-                 tabPanel('Results Table', dataTableOutput('mastresults')),
-                 tabPanel("Adaptive thresholding", plotOutput("threshplot")),
-                 tabPanel("Violin Plot", plotOutput("hurdleviolin")),
-                 tabPanel("Linear Model", plotOutput("hurdlelm")),
-                 tabPanel("Heatmap", plotOutput("hurdleHeatmap"))
-               ))
+        column(
+          4,
+          wellPanel(
+            sliderInput("FCthreshold", "Select fold change threshold", log2(1), log2(2), 0.6),
+            h3("Filtering"),
+            sliderInput("hurdlethresh", "Select expression threshold", 0, 1, 0.1),
+            sliderInput("samplesize", "Select Sample Size", 0, 500, 100),
+            selectInput("hurdlecondition","Select Condition for Hurdle Model",clusterChoice),
+            uiOutput("hurdleconditionofinterestUI"),
+            sliderInput("hurdlepvalue", "p-value cutoff:", 0.01, 0.2, 0.05),
+            selectInput("selectCorrection","Correction Methods",c("FDR", "Bonferroni")),
+            withBusyIndicatorUI(actionButton("runDEhurdle", "Run DE Using Hurdle")),
+            h5("Download Results"),
+            downloadButton("downloadHurdlerResult","Download Results")
+          )
+        ),
+        column(
+          8,
+          tabsetPanel(
+            tabPanel('Results Table', dataTableOutput('mastresults')),
+            tabPanel("Adaptive thresholding", plotOutput("threshplot")),
+            tabPanel("Violin Plot", plotOutput("hurdleviolin")),
+            tabPanel("Linear Model", plotOutput("hurdlelm")),
+            tabPanel("Heatmap", plotOutput("hurdleHeatmap"))
+          )
+        )
       )
     )
   )
