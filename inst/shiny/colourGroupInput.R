38983b793f7ff692cafb1b0c7fe95a87e7360096
stc1 <stc@bu.edu>
2017-06-03 19:21:17 -0400

    Fixed glitch in colourGroup module

--- inst/shiny/colourGroupInput.R
+++ inst/shiny/colourGroupInput.R
@@ -17,15 +17,21 @@ colourGroup = function(input, output, session, heading = "", options="", labels 
   ids = reactive(sapply(options, function (option) paste(option, "inputId", sep="_")))
 
   cols <- reactive({
-    sapply(1:length(options), function (i) {
-      if (length(options)!=0) {
+    if (length(options)!=0){
+      sapply(1:length(options), function (i) {
         if (!is.null(input[[as.character(ids()[i])]])){
           setNames(input[[as.character(ids()[i])]], options[i])
         }
-      }
-    })
-  })
-
+      })
+    }
+  }) 
+  
+  if (length(cols)==0){
+    cols2 = NULL
+  } else {
+    cols2 = debounce(cols, 2000)
+  }
+  
   output$heading = renderText({heading})
   
   output$colorChoosers = renderUI({
@@ -37,13 +43,13 @@ colourGroup = function(input, output, session, heading = "", options="", labels 
         } else {
           color = input[[as.character(ids()[i])]]
         }
-        L[[i]] = list(do.call(colourpicker::colourInput, 
+        L[[i]] = list(do.call(colourpicker::colourInput,
                          list(ns(ids()[i]), label = options[i], value = color, ...)))
       }
       return(L)
     }
-    
+
   })
   
-  return(cols)
+  return(cols2)
 }
