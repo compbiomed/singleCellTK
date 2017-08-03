040d27f182f3a78985193913b82c92b816288d04
David Jenkins <d.jenkins0@gmail.com>
2017-08-03 18:34:14 -0400

    Moved example data to singlecellexperiment and misc_functions. This version of the toolkit won't work very well because I removed scater but some of the files haven't been converted yet. Follow the progress spreadsheet and test each function individually before merging to master

--- R/data.R
+++ R/data.R
@@ -1,4 +1,5 @@
-#' Example Single Cell RNA-Seq data in SCESet Object, GSE60361 subest
+#' Example Single Cell RNA-Seq data in SingleCellExperiment Object, GSE60361
+#' subest
 #'
 #' A subset of 30 samples from a single cell RNA-Seq experiment from Zeisel, et
 #' al. Science 2015. The data was produced from cells from the mouse
@@ -6,93 +7,67 @@
 #' identified as oligodendrocytes and 15 of the cell were identified as
 #' microglia.
 #'
-#' @name GSE60361_subset
+#' @name GSE60361_subset_sce
 #' @docType data
-#' @format List of two data frames, with counts and annotations. Use them as
-#' input to createSCESet()
+#' @format SingleCellExperiment
 #' @source DOI: 10.1126/science.aaa1934
 #' @keywords datasets
 #' @examples
-#' library(scater)
-#' data("GSE60361_subset")
-#' GSE60361_SCESet <- createSCESet(countfile = GSE60361_subset$counts,
-#'                                 annotfile = GSE60361_subset$annot,
-#'                                 inputdataframes = TRUE)
-"GSE60361_subset"
+#' data("GSE60361_subset_sce")
+"GSE60361_subset_sce"
 
-#' Example Single Cell RNA-Seq data in SCESet Object, GSE73121
+#' Example Single Cell RNA-Seq data in SingleCellExperiment Object, GSE73121
 #'
 #' 117 Single-cell transcriptome profiling for metastatic renal cell carcinoma
 #' patient-derived cells
 #'
-#' @name GSE73121
+#' @name GSE73121_sce
 #' @docType data
-#' @format List of two data frames, with counts and annotations. Use them as
-#' input to createSCESet()
+#' @format SingleCellExperiment
 #' @source DOI: 10.1186/s13059-016-0945-9
 #' @keywords datasets
 #' @examples
-#' library(scater)
-#' data("GSE73121")
-#' GSE73121_SCESet <- createSCESet(countfile = GSE73121$counts,
-#'                                 annotfile = GSE73121$annot,
-#'                                 inputdataframes = TRUE)
-"GSE73121"
+#' data("GSE73121_sce")
+"GSE73121_sce"
 
-#' Example Single Cell RNA-Seq data in SCESet Object, GSE66507
+#' Example Single Cell RNA-Seq data in SingleCellExperiment Object, GSE66507
 #'
 #' 30 Single-cells from embryonic stem cells separated into three different
 #' tissue types.
 #'
-#' @name GSE66507
+#' @name GSE66507_sce
 #' @docType data
-#' @format List of two data frames, with counts and annotations. Use them as
-#' input to createSCESet()
+#' @format SingleCellExperiment
 #' @source DOI: 10.1242/dev.123547
 #' @keywords datasets
 #' @examples
-#' library(scater)
-#' data("GSE66507")
-#' GSE66507_SCESet <- createSCESet(countfile = GSE66507$counts,
-#'                                 annotfile = GSE66507$annot,
-#'                                 inputdataframes = TRUE)
-"GSE66507"
+#' data("GSE66507_sce")
+"GSE66507_sce"
 
-#' Example Single Cell RNA-Seq data in SCESet Object, GSE36552
+#' Example Single Cell RNA-Seq data in SingleCellExperiment Object, GSE36552
 #'
 #' 124 Single-cell transcriptome profiling from embryonic stem cells derived
 #' from donated human pre-implatation embryos.
 #'
-#' @name GSE36552
+#' @name GSE36552_sce
 #' @docType data
-#' @format List of two data frames, with counts and annotations. Use them as
-#' input to createSCESet()
+#' @format SingleCellExperiment
 #' @source DOI: 10.1038/nsmb.2660
 #' @keywords datasets
 #' @examples
-#' library(scater)
-#' data("GSE36552")
-#' GSE36552_SCESet <- createSCESet(countfile = GSE36552$counts,
-#'                                 annotfile = GSE36552$annot,
-#'                                 inputdataframes = TRUE)
-"GSE36552"
+#' data("GSE36552_sce")
+"GSE36552_sce"
 
 #' Example Single Cell RNA-Seq MAITS data from MAST package
 #'
 #' 96 Single-cell transcriptome profiling from Mucosal Associated Invariant T
 #' cells (MAITs), measured on the Fluidigm C1.
 #'
-#' @name maits_SCESet
+#' @name maits_sce
 #' @docType data
-#' @format List of three data frames, with counts, features and annotations.
-#' Use them as input to createSCESet()
+#' @format SingleCellExperiment
 #' @source DOI: 10.1186/s13059-015-0844-5
 #' @keywords datasets
 #' @examples
-#' library(scater)
-#' data("maits_SCESet")
-#' maits_SCESet <- createSCESet(countfile = maits_SCESet$counts,
-#'                              annotfile = maits_SCESet$annot,
-#'                              featurefile = maits_SCESet$features,
-#'                              inputdataframes = TRUE)
-"maits_SCESet"
+#' data("maits_sce")
+"maits_sce"
--- R/misc_functions.R
+++ R/misc_functions.R
@@ -1,8 +1,8 @@
-#' Summarize SCESet
+#' Summarize SingleCellExperiment
 #'
-#' Creates a table of summary metrics from an input SCESet.
+#' Creates a table of summary metrics from an input SingleCellExperiment.
 #'
-#' @param indata Input SCESet
+#' @param indata Input SingleCellExperiment
 #'
 #' @return A data.frame object of summary metrics.
 #' @export summarizeTable
@@ -15,16 +15,16 @@ summarizeTable <- function(indata){
                                "Genes with no expression across all samples"),
                     "Value" = c(ncol(indata),
                               nrow(indata),
-                              as.integer(mean(apply(scater::counts(indata), 2, function(x) sum(x)))),
-                              as.integer(mean(apply(scater::counts(indata), 2, function(x) sum(x > 0)))),
-                              sum(apply(scater::counts(indata), 2, function(x) sum(as.numeric(x) == 0)) < 1700),
-                              sum(rowSums(scater::counts(indata)) == 0))))
+                              as.integer(mean(apply(assay(indata, "counts"), 2, function(x) sum(x)))),
+                              as.integer(mean(apply(assay(indata, "counts"), 2, function(x) sum(x > 0)))),
+                              sum(apply(assay(indata, "counts"), 2, function(x) sum(as.numeric(x) == 0)) < 1700),
+                              sum(rowSums(assay(indata, "counts")) == 0))))
 }
 
-#' Create a SCESet object
+#' Create a SingleCellExperiment object
 #'
-#' From a file of counts and a file of annotation information, create a SCESet
-#' object.
+#' From a file of counts and a file of annotation information, create a
+#' SingleCellExperiment object.
 #'
 #' @param countfile The path to a text file that contains a header row of sample
 #' names, and rows of raw counts per gene for those samples.
@@ -37,16 +37,15 @@ summarizeTable <- function(indata){
 #' @param inputdataframes If TRUE, countfile and annotfile are read as data
 #' frames instead of file paths. The default is FALSE.
 #'
-#' @return a SCESet object
-#' @export createSCESet
+#' @return a SingleCellExperiment object
+#' @export createSCE
 #' @examples
-#' library(scater)
-#' data("GSE60361_subset")
-#' GSE60361_SCESet <- createSCESet(countfile = GSE60361_subset$counts,
-#'                                 annotfile = GSE60361_subset$annot,
-#'                                 inputdataframes = TRUE)
-createSCESet <- function(countfile=NULL, annotfile=NULL, featurefile=NULL,
-                         inputdataframes=FALSE){
+#' \dontrun{
+#' GSE60361_sce <- createSCE(countfile = "/path/to/input_counts.txt",
+#'                           annotfile = "/path/to/input_annots.txt")
+#'}
+createSCE <- function(countfile=NULL, annotfile=NULL, featurefile=NULL,
+                      inputdataframes=FALSE){
   if (is.null(countfile)){
     stop("You must supply a count file.")
   }
@@ -66,15 +65,16 @@ createSCESet <- function(countfile=NULL, annotfile=NULL, featurefile=NULL,
   if (is.null(annotfile)){
     annotin <- data.frame(row.names = colnames(countsin))
     annotin$Sample <- rownames(annotin)
+    annotin <- DataFrame(annotin)
   }
   if (is.null(featurefile)){
     featurein <- data.frame(Gene = rownames(countsin))
     rownames(featurein) <- featurein$Gene
+    featurein <- DataFrame(featurein)
   }
-  pd <- methods::new("AnnotatedDataFrame", data = annotin)
-  fd <- methods::new("AnnotatedDataFrame", data = featurein)
-  return(scater::newSCESet(countData = countsin, phenoData = pd,
-                           featureData = fd))
+  return(SummarizedExperiment(assays=list(counts=as.matrix(countsin)),
+                              colData=annotin,
+                              rowData=featurein))
 }
 
 #' Filter Genes and Samples from a Single Cell Object
@@ -92,22 +92,18 @@ createSCESet <- function(countfile=NULL, annotfile=NULL, featurefile=NULL,
 #' @export filterSCData
 #'
 #' @examples
-#' library(scater)
-#' data("GSE60361_subset")
-#' GSE60361_SCESet <- createSCESet(countfile = GSE60361_subset$counts,
-#'                                 annotfile = GSE60361_subset$annot,
-#'                                 inputdataframes = TRUE)
-#' GSE60361_SCESet_filtered <- filterSCData(GSE60361_SCESet,
-#'                                          deletesamples="X1772063061_G11")
+#' data("GSE60361_subset_sce")
+#' GSE60361_subset_sce <- filterSCData(GSE60361_subset_sce,
+#'                                     deletesamples="X1772063061_G11")
 filterSCData <- function(insceset, deletesamples=NULL, remove_noexpress=TRUE,
                          remove_bottom=0.5, minimum_detect_genes=1700){
   insceset <- insceset[, !(colnames(insceset) %in% deletesamples)]
   if (remove_noexpress){
-    insceset <- insceset[rowSums(counts(insceset)) != 0, ]
+    insceset <- insceset[rowSums(assay(insceset, "counts")) != 0, ]
   }
   nkeeprows <- ceiling((1 - remove_bottom) * as.numeric(nrow(insceset)))
-  tokeeprow <- order(rowSums(counts(insceset)), decreasing = TRUE)[1:nkeeprows]
-  tokeepcol <- apply(counts(insceset), 2, function(x) sum(as.numeric(x) == 0)) >= minimum_detect_genes
+  tokeeprow <- order(rowSums(assay(insceset, "counts")), decreasing = TRUE)[1:nkeeprows]
+  tokeepcol <- apply(assay(insceset, "counts"), 2, function(x) sum(as.numeric(x) == 0)) >= minimum_detect_genes
   insceset <- insceset[tokeeprow, tokeepcol]
   return(insceset)
 }
