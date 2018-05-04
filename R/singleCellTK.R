239fa8a8e2a7112de8d19ba17784efebb97ea580
David Jenkins <d.jenkins0@gmail.com>
2018-05-04 14:24:23 -0400

    Ability to hide version number in the GUI

--- R/singleCellTK.R
+++ R/singleCellTK.R
@@ -3,6 +3,8 @@
 #' Use this function to run the single cell analysis app.
 #'
 #' @param inSCE The input SCtkExperiment class object
+#' @param includeVersion Include the version number in the SCTK header. The
+#' default is TRUE.
 #'
 #' @import GSVAdata Biobase DelayedArray
 #'
@@ -20,11 +22,12 @@
 #'   singleCellTK(mouseBrainSubsetSCE)
 #' }
 #'
-singleCellTK <- function(inSCE=NULL) {
+singleCellTK <- function(inSCE=NULL, includeVersion=TRUE) {
   appDir <- system.file("shiny", package = "singleCellTK")
   if (!is.null(inSCE) & is.null(rownames(inSCE))){
     stop("ERROR: No row names (gene names) found.")
   }
   shiny::shinyOptions(inputSCEset = inSCE)
+  shiny::shinyOptions(includeVersion = includeVersion)
   shiny::runApp(appDir, display.mode = "normal")
 }
