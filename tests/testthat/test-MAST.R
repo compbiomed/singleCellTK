83c9437ec5a5e47862ef5030d27918fb9c5a9703
Adam Lewis <adlewis@Adam-HP.localdomain>
2018-06-28 16:16:20 -0400

    made test code create sce object

--- tests/testthat/test-MAST.R
+++ tests/testthat/test-MAST.R
@@ -1,5 +1,14 @@
 context("MAST testing")
 
+data(maits, package="MAST")
+maits_sce <- createSCE(assayFile = t(maits$expressionmat),
+                       annotFile = maits$cdat,
+                       featureFile = maits$fdat,
+                       assayName = "logtpm",
+                       inputDataFrames = TRUE,
+                       createLogCounts = FALSE)
+rm(maits)
+
 test_that("MAST should fail if too few samples pass filter", {
   expect_error(MAST(maits_sce, condition = "condition", freqExpressed = "1", useThresh = TRUE, useAssay = "logtpm"),
                "Not enough genes pass frequency expressed filter of 1")
