cbbb299650ca38243f1d23884bdda8cc05359ef0
Ada <ada.mcfarlane@gmail.com>
2018-07-05 13:21:56 -0400

    Sidebar - added main sidebar heading and standardized sub-headings format

--- inst/shiny/ui_02_filter.R
+++ inst/shiny/ui_02_filter.R
@@ -7,35 +7,45 @@ shinyPanelFilter <- fluidPage(
               "(help)", target = "_blank")),
     sidebarLayout(
       sidebarPanel(
+        h3("Settings:"),
+        
+        h4("Assay Settings"),
         selectInput("filterAssaySelect", "Select Assay:", currassays),
         checkboxInput("removeNoexpress", "Remove genes with 0 expression across all samples (Recommended)", value = TRUE),
         numericInput("minDetectGene", label = "Minimum Detected Genes per Sample.", value = 1700, min = 1, max = 100000),
         numericInput("LowExpression", "% Low Gene Expression to Filter", value = 40, min = 0, max = 100),
-        h2("Delete Outliers"),
+        tags$hr(),
+        
+        h4("Delete Outliers"),
         selectInput("deletesamplelist", "Select Samples:",
                     sampleChoice,
                     multiple = TRUE),
         withBusyIndicatorUI(actionButton("filterData", "Filter Data")),
         actionButton("resetData", "Reset"),
         tags$hr(),
-        h3("Filter samples by annotation"),
+        
+        h4("Filter samples by annotation"),
         selectInput("filteredSample", "Select Annotation:", c("none", clusterChoice)),
         uiOutput("filterSampleOptions"),
         tags$hr(),
-        h3("Filter genes by feature annotation"),
+        
+        h4("Filter genes by feature annotation"),
         selectInput("filteredFeature", "Select Feature:", c("none", featureChoice)),
         uiOutput("filterFeatureOptions"),
         tags$hr(),
-        h3("Convert gene annotations"),
+        
+        h4("Convert gene annotations"),
         selectInput("orgOrganism", "Select Organism:", as.character(grep("^org\\.", installed.packages()[, "Package"], value = TRUE))),
         uiOutput("orgConvertColumns"),
         withBusyIndicatorUI(actionButton("convertGenes", "Convert")),
         tags$hr(),
-        h3("Delete an annotation column:"),
+        
+        h4("Delete an annotation column"),
         selectInput("deleterowdatacolumn", "Annotation Column:", clusterChoice),
         actionButton("deleterowDatabutton", "Delete Column"),
         tags$hr(),
-        h3("Randomly Subset"),
+        
+        h4("Randomly Subset"),
         numericInput("downsampleNum", "Number of samples to keep:", min = 2,
                      max = numSamples, value = numSamples, step = 1),
         withBusyIndicatorUI(actionButton("downsampleGo", "Subset Data")),
