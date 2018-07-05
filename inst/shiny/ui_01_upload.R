7c28ca856db902f5e6f0fd9e758ddec63b3c2660
Ada <ada.mcfarlane@gmail.com>
2018-07-05 12:43:03 -0400

    Put the three file upload sections into wellPanels (visual grouping element)

--- inst/shiny/ui_01_upload.R
+++ inst/shiny/ui_01_upload.R
@@ -41,27 +41,29 @@ shinyPanelUpload <- fluidPage(
       h3("Upload data in tab separated text format:"),
       fluidRow(
         column(width = 4,
-          h4("Example count file:"),
-          HTML('<table class="table"><thead><tr class="header"><th>Gene</th>
-               <th>Cell1</th><th>Cell2</th><th>&#x2026;</th><th>CellN</th>
-               </tr></thead><tbody><tr class="odd"><td>Gene1</td><td>0</td>
-               <td>0</td><td>&#x2026;</td><td>0</td></tr><tr class="even">
-               <td>Gene2</td><td>5</td><td>6</td><td>&#x2026;</td><td>0</td>
-               </tr><tr class="odd"><td>Gene3</td><td>4</td><td>3</td>
-               <td>&#x2026;</td><td>8</td></tr><tr class="even">
-               <td>&#x2026;</td><td>&#x2026;</td><td>&#x2026;</td>
-               <td>&#x2026;</td><td>&#x2026;</td></tr><tr class="odd">
-               <td>GeneM</td><td>10</td><td>10</td><td>&#x2026;</td><td>10</td>
-               </tr></tbody></table>'),
-          tags$a(href = "https://drive.google.com/open?id=1n0CtM6phfkWX0O6xRtgPPg6QuPFP6pY8",
-                 "Download an example count file here.", target = "_blank"),
-          tags$br(),
-          tags$br(),
-          fileInput(
-            "countsfile", "Input assay (eg. counts, required):",
-            accept = c(
-              "text/csv", "text/comma-separated-values",
-              "text/tab-separated-values", "text/plain", ".csv", ".tsv"
+          wellPanel(
+            h4("Example count file:"),
+            HTML('<table class="table"><thead><tr class="header"><th>Gene</th>
+                 <th>Cell1</th><th>Cell2</th><th>&#x2026;</th><th>CellN</th>
+                 </tr></thead><tbody><tr class="odd"><td>Gene1</td><td>0</td>
+                 <td>0</td><td>&#x2026;</td><td>0</td></tr><tr class="even">
+                 <td>Gene2</td><td>5</td><td>6</td><td>&#x2026;</td><td>0</td>
+                 </tr><tr class="odd"><td>Gene3</td><td>4</td><td>3</td>
+                 <td>&#x2026;</td><td>8</td></tr><tr class="even">
+                 <td>&#x2026;</td><td>&#x2026;</td><td>&#x2026;</td>
+                 <td>&#x2026;</td><td>&#x2026;</td></tr><tr class="odd">
+                 <td>GeneM</td><td>10</td><td>10</td><td>&#x2026;</td><td>10</td>
+                 </tr></tbody></table>'),
+            tags$a(href = "https://drive.google.com/open?id=1n0CtM6phfkWX0O6xRtgPPg6QuPFP6pY8",
+                   "Download an example count file here.", target = "_blank"),
+            tags$br(),
+            tags$br(),
+            fileInput(
+              "countsfile", "Input assay (eg. counts, required):",
+              accept = c(
+                "text/csv", "text/comma-separated-values",
+                "text/tab-separated-values", "text/plain", ".csv", ".tsv"
+              )
             )
           ),
           selectInput("inputAssayType", "Input Assay Type:", c("counts",
@@ -72,44 +74,48 @@ shinyPanelUpload <- fluidPage(
           checkboxInput("createLogcounts", "Also create log2 input assay on upload", value = TRUE)
         ),
         column(width = 4,
-          h4("Example sample annotation file:"),
-          HTML('<table class="table"><thead><tr class="header"><th>Cell</th>
-               <th>Annot1</th><th>…</th></tr></thead><tbody><tr class="odd">
-               <td>Cell1</td><td>a</td><td>…</td></tr><tr class="even">
-               <td>Cell2</td><td>a</td><td>…</td></tr><tr class="odd">
-               <td>Cell3</td><td>b</td><td>…</td></tr><tr class="even">
-               <td>…</td><td>…</td><td>…</td></tr><tr class="odd"><td>CellN</td>
-               <td>b</td><td>…</td></tr></tbody></table>'),
-          tags$a(href = "https://drive.google.com/open?id=10IDmZQUiASN4wnzO4-WRJQopKvxCNu6J",
-                 "Download an example annotation file here.", target = "_blank"),
-          tags$br(),
-          tags$br(),
-          fileInput(
-            "annotFile", "Sample annotations (optional):",
-            accept = c(
-              "text/csv", "text/comma-separated-values",
-              "text/tab-separated-values", "text/plain", ".csv", ".tsv"
+          wellPanel(     
+            h4("Example sample annotation file:"),
+            HTML('<table class="table"><thead><tr class="header"><th>Cell</th>
+                 <th>Annot1</th><th>…</th></tr></thead><tbody><tr class="odd">
+                 <td>Cell1</td><td>a</td><td>…</td></tr><tr class="even">
+                 <td>Cell2</td><td>a</td><td>…</td></tr><tr class="odd">
+                 <td>Cell3</td><td>b</td><td>…</td></tr><tr class="even">
+                 <td>…</td><td>…</td><td>…</td></tr><tr class="odd"><td>CellN</td>
+                 <td>b</td><td>…</td></tr></tbody></table>'),
+            tags$a(href = "https://drive.google.com/open?id=10IDmZQUiASN4wnzO4-WRJQopKvxCNu6J",
+                   "Download an example annotation file here.", target = "_blank"),
+            tags$br(),
+            tags$br(),
+            fileInput(
+              "annotFile", "Sample annotations (optional):",
+              accept = c(
+                "text/csv", "text/comma-separated-values",
+                "text/tab-separated-values", "text/plain", ".csv", ".tsv"
+              )
             )
           )
         ),
         column(width = 4,
-          h4("Example feature file:"),
-          HTML('<table class="table"><thead><tr class="header"><th>Gene</th>
-               <th>Annot2</th><th>…</th></tr></thead><tbody><tr class="odd">
-               <td>Gene1</td><td>a</td><td>…</td></tr><tr class="even">
-               <td>Gene2</td><td>a</td><td>…</td></tr><tr class="odd">
-               <td>Gene3</td><td>b</td><td>…</td></tr><tr class="even">
-               <td>…</td><td>…</td><td>…</td></tr><tr class="odd"><td>GeneM</td>
-               <td>b</td><td>…</td></tr></tbody></table>'),
-          tags$a(href = "https://drive.google.com/open?id=1gxXaZPq5Wrn2lNHacEVaCN2a_FHNvs4O",
-                "Download an example feature file here.", target = "_blank"),
-          tags$br(),
-          tags$br(),
-          fileInput(
-            "featureFile", "Feature annotations (optional):",
-            accept = c(
-              "text/csv", "text/comma-separated-values",
-              "text/tab-separated-values", "text/plain", ".csv", ".tsv"
+          wellPanel(
+            h4("Example feature file:"),
+            HTML('<table class="table"><thead><tr class="header"><th>Gene</th>
+                 <th>Annot2</th><th>…</th></tr></thead><tbody><tr class="odd">
+                 <td>Gene1</td><td>a</td><td>…</td></tr><tr class="even">
+                 <td>Gene2</td><td>a</td><td>…</td></tr><tr class="odd">
+                 <td>Gene3</td><td>b</td><td>…</td></tr><tr class="even">
+                 <td>…</td><td>…</td><td>…</td></tr><tr class="odd"><td>GeneM</td>
+                 <td>b</td><td>…</td></tr></tbody></table>'),
+            tags$a(href = "https://drive.google.com/open?id=1gxXaZPq5Wrn2lNHacEVaCN2a_FHNvs4O",
+                  "Download an example feature file here.", target = "_blank"),
+            tags$br(),
+            tags$br(),
+            fileInput(
+              "featureFile", "Feature annotations (optional):",
+              accept = c(
+                "text/csv", "text/comma-separated-values",
+                "text/tab-separated-values", "text/plain", ".csv", ".tsv"
+              )
             )
           )
         )
