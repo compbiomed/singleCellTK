d9a081809cd9a80c3ef95157ae8bbe177e60eba3
David Jenkins <d.jenkins0@gmail.com>
2018-11-01 16:08:21 -0400

    fix potential lintr issue

--- tests/testthat/test-lint.R
+++ tests/testthat/test-lint.R
@@ -1,15 +1,17 @@
-if (requireNamespace("lintr", quietly = TRUE) & packageVersion("lintr") >= "1.0.2.9000") {
-  context("lints")
-  test_that("Package Style", {
-    lintr::expect_lint_free(linters = c(lintr::trailing_whitespace_linter,
-                                        lintr::no_tab_linter,
-                                        lintr::T_and_F_symbol_linter,
-                                        lintr::semicolon_terminator_linter,
-                                        lintr::infix_spaces_linter,
-                                        lintr::closed_curly_linter,
-                                        lintr::assignment_linter,
-                                        lintr::commas_linter,
-                                        lintr::seq_linter,
-                                        lintr::spaces_left_parentheses_linter))
-  })
+if (requireNamespace("lintr", quietly = TRUE)) {
+  if(packageVersion("lintr") >= "1.0.2.9000") {
+    context("lints")
+    test_that("Package Style", {
+      lintr::expect_lint_free(linters = c(lintr::trailing_whitespace_linter,
+                                          lintr::no_tab_linter,
+                                          lintr::T_and_F_symbol_linter,
+                                          lintr::semicolon_terminator_linter,
+                                          lintr::infix_spaces_linter,
+                                          lintr::closed_curly_linter,
+                                          lintr::assignment_linter,
+                                          lintr::commas_linter,
+                                          lintr::seq_linter,
+                                          lintr::spaces_left_parentheses_linter))
+    })
+  }
 }
