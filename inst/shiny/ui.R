a41424633acc613ade4371d1aad967f068baf0b2
David Jenkins <d.jenkins0@gmail.com>
2019-01-16 22:49:58 -0500

    Make theme configurable

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -64,6 +64,12 @@ if (!is.null(getShinyOption("inputSCEset"))){
                     </button></div>")
 }
 
+if(is.null(getShinyOption("theme"))){
+  shinyTheme <- "flatly"
+} else {
+  shinyTheme <- getShinyOption("theme")
+}
+
 source("ui_01_upload.R", local = TRUE) #creates shinyPanelUpload variable
 source("ui_02_filter.R", local = TRUE) #creates shinyPanelFilter variable
 source("ui_03_cluster.R", local = TRUE) #creates shinyPanelCluster variable
@@ -90,7 +96,7 @@ if (is.null(getShinyOption("includeVersion"))){
 shinyUI(
   navbarPage(
     tooltitle,
-    theme = shinytheme("flatly"),
+    theme = shinytheme(shinyTheme),
     #Upload Tab
     tabPanel("Upload", shinyPanelUpload),
     tabPanel("Data Summary & Filtering", shinyPanelFilter),
