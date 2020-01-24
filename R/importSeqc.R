2a9a98ba57912750a23505e39dc11d6078d807b4
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2020-01-23 22:11:21 -0500

    Fixed column_name header

--- R/importSeqc.R
+++ R/importSeqc.R
@@ -11,7 +11,7 @@
         assays = list(counts = matrix))
     SummarizedExperiment::rowData(sce) <- features
     SummarizedExperiment::colData(sce) <- S4Vectors::DataFrame(
-        cell_barcode = barcodes[[1]],
+        cell_barcode = as.character(barcodes[[1]]),
         column_name = coln,
         sample = sampleName,
         row.names = coln)
