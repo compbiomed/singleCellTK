f1d6a29457a117f598151717f404f8f6a48d2906
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2020-01-23 19:23:09 -0500

    removed unnecessary messages

--- R/importSeqc.R
+++ R/importSeqc.R
@@ -78,13 +78,11 @@
 
         cb[[i]] <- .readBarcodes(file.path(dir, barcodesFile))
         if (isTRUE(cbNotFirstCol)) {
-            message("First column of barcode file was row index and it was removed.")
             cb[[i]] <- cb[[i]][, -1, drop = FALSE]
         }
 
         fe[[i]] <- .readFeatures(file.path(dir, featuresFile))
         if (isTRUE(feNotFirstCol)) {
-            message("First column of gene file was row index and it was removed.")
             fe[[i]] <- fe[[i]][, -1, drop = FALSE]
         }
 
