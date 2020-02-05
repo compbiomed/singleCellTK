c02fdf1b154e79e119e4fb7e36ad9c07afb3a638
Joshua D. Campbell <joshua.d.campbell@gmail.com>
2020-02-05 11:26:46 -0500

    Added functionality/support for reticulate

--- R/reticulate_setup.R
+++ R/reticulate_setup.R
@@ -8,7 +8,21 @@ scrublet <- NULL
 scipy <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  # delay load foo module (will only be loaded when accessed via $)
-  scrublet <<- import("scrublet", delay_load = TRUE)
-  scipy <<- import("scipy", delay_load = TRUE)
+  # use superassignment to update global reference to scipy
+  scrublet <<- reticulate::import("scrublet", delay_load = TRUE)
+  scipy <<- reticulate::import("scipy", delay_load = TRUE)
+}
+
+
+#' @name sctk_py_install
+#' @title Installs Python packages
+#' @description Install all Python packages used in the \code{\link{singleCellTK}} package
+#' using \code{\link[reticulate]{py_install}} from package \code{\link{reticulate}}.
+#' @param packages List of packages to install
+#' @param ... Other parameters to pass to \code{\link[reticulate]{py_install}}. Can be useful for
+#' selecting which environment to install these packages into. If no parameters are supplied,
+#' then packages will be installed into default Python environment chosen by \code{\link{reticulate}}.
+#' @export
+sctk_py_install = function(packages = c("scipy", "scrublet"), ...) {
+  reticulate::py_install(packages, ...)
 }
