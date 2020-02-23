1a542e91687c84e760a0a88a493b1ae7076627f1
Shruthi Bandyadka <shruthibhat93@gmail.com>
2020-02-23 15:42:50 -0500

    Modifying importDropEst to loop through multiple samples

--- R/importDropEst.R
+++ R/importDropEst.R
@@ -1,6 +1,6 @@
 
-.readDropEstFile <- function(dropEstdirs, counts_matrix_type,rdsFileName){
-  dropEst_cell_counts <- paste0(dropEstdirs,'/',rdsFileName,'.rds')
+.readDropEstFile <- function(sampleDir, dataType,rdsFileName){
+  dropEst_cell_counts <- file.path(sampleDir, paste(rdsFileName, '.rds', sep=''))
   if (!file.exists(dropEst_cell_counts)){
     stop("DropEst output not found at location specified. Please check path provided and/or filename.")
   }
@@ -9,7 +9,7 @@
   return(dropEst_rds)
 }
 
-.constructColdata <- function(dropEst_rds,counts_matrix, counts_matrix_type){
+.constructColdata <- function(dropEst_rds,counts_matrix, dataType){
   coldata_fields <- c("mean_reads_per_umi","aligned_reads_per_cell","aligned_umis_per_cell","requested_umis_per_cb","requested_reads_per_cb")
   coldata_df <-  list()
   for (field in coldata_fields){
@@ -17,12 +17,13 @@
       coldata_field_df <- data.frame(as.matrix(dropEst_rds[[field]]))
       names(coldata_field_df)[1] <- field
       coldata_field_df$cell <- row.names(coldata_field_df)
+      
       coldata_df[[field]] <- coldata_field_df
     }}
   coldata_df_merged <- Reduce(function(x, y) merge(x, y, all=TRUE,by="cell"), coldata_df)
   row.names(coldata_df_merged) <- coldata_df_merged$cell
   coldata_df_merged <- S4Vectors::DataFrame(as.matrix(coldata_df_merged))
-  if (counts_matrix_type == 'filtered'){
+  if (dataType == 'filtered'){
     coldata_df_merged <- coldata_df_merged[coldata_df_merged$cell %in% colnames(counts_matrix),]
   }
   return(coldata_df_merged)
@@ -38,21 +39,27 @@
   return(metadata)
 }
 
-.importDropEst <- function(dropEstdirs, counts_matrix_type, rdsFileName){
+.importDropEstSample <- function(sampleDir = './', 
+                                 dataType, 
+                                 rdsFileName, 
+                                 sampleName = 'sample'){
   ## Read DropEst RDS
-  dropEst_rds <- .readDropEstFile(dropEstdirs,counts_matrix_type,rdsFileName)
-  if (counts_matrix_type == 'filtered' && 'cm' %in% names(dropEst_rds)) {
+  dropEst_rds <- .readDropEstFile(sampleDir,dataType,rdsFileName)
+  if (dataType == 'filtered' && 'cm' %in% names(dropEst_rds)) {
     counts_matrix <- dropEst_rds$cm
-  } else if (counts_matrix_type == 'raw' && 'cm_raw' %in% names(dropEst_rds)) {
+  } else if (dataType == 'raw' && 'cm_raw' %in% names(dropEst_rds)) {
     counts_matrix <- dropEst_rds$cm_raw
   } else {
     stop("No counts matrix found in the .rds provided! Exiting.")
   }
   ## Create SingleCellExperiment object
-  sce <- SingleCellExperiment::SingleCellExperiment(assays = list(counts = counts_matrix))
-  
   ## Add SCE ColData. If using filtered counts matrix, colData is subset to include filtered cells.
-  sce_coldata <- .constructColdata(dropEst_rds, counts_matrix, counts_matrix_type)
+  ## append sample name to cells in SCE 
+  sce <- SingleCellExperiment::SingleCellExperiment(assays = list(counts = counts_matrix))
+  colnames(sce) <- paste0(sampleName,"_",colnames(sce)) 
+  sce_coldata <- .constructColdata(dropEst_rds, counts_matrix, dataType)
+  row.names(sce_coldata) <- paste0(sampleName,"_",row.names(sce_coldata))
+  
   if (dim(counts_matrix)[2] == dim(sce_coldata)[1]){
     SummarizedExperiment::colData(sce) <- sce_coldata
   } else {
@@ -73,8 +80,9 @@
 #' create a SingleCellExperiment object from either the raw or filtered counts matrix.
 #' Additionally parse through the RDS to obtain appropriate feature annotations as 
 #' SCE coldata, in addition to any metadata.
-#' @param dropEstdirs Path to the folder containing the DropEst cell.counts.rds output
-#' @param counts_matrix_type can be "filtered" or "raw". Default is "filtered"
+#' @param sampleDirs vector of Paths to the folders containing the DropEst cell.counts.rds output
+#' #' @param sampleNames vector of sample names that will be appended to cell names of each SCE
+#' @param dataType can be "filtered" or "raw". Default is "filtered"
 #' @param rdsFileName File name prefix of the DropEst RDS output. default is "cell.counts"
 #' @details
 #' \code{importDropEst} expects either raw counts matrix stored as "cm_raw" or filtered
@@ -92,16 +100,31 @@
 #' Example DropEst outputs were downloaded from the DropEst Github 
 #' (http://pklab.med.harvard.edu/viktor/dropest_paper/dropest_0.8.5.zip). 
 #' To run the dropest import function with the example dataset, 
-#' set the dropEstdirs variable to the example dropEst provided in SCTK as follows-
-#' sce <- importDropEst(dropEstdirs = 'path/to/dropest/folder/', 
-#'                      counts_matrix_type='filtered')
+#' set the sampleDirs variable to the example dropEst provided in SCTK as follows-
+#' sce <- importDropEst(sampleDirs = c('path/to/dropest/folder/'), 
+#'                      dataType='filtered', sampleNames=c('sample'))
 #' @export
-importDropEst <- function(dropEstdirs = NULL, 
-                          counts_matrix_type = 'filtered',
-                          rdsFileName = 'cell.counts') {
-  .importDropEst(dropEstdirs = dropEstdirs, 
-                 counts_matrix_type = counts_matrix_type,
-                 rdsFileName = rdsFileName)
+importDropEst <- function(sampleDirs = NULL, 
+                          dataType = c('filtered','raw'),
+                          rdsFileName = 'cell.counts',
+                          sampleNames = NULL) {
+  dataType <- match.arg(dataType)
+  
+  if (length(sampleDirs)!=length(sampleNames)){
+    stop("Please provide sample names for all input directories")
+  }
+  
+  res <- vector("list", length = length(sampleDirs))
+  
+  for (i in seq_along(sampleDirs)){
+    scei <- .importDropEstSample(sampleDir = sampleDirs[[i]],
+                         sampleName = sampleNames[[i]],
+                         dataType = dataType,
+                         rdsFileName = rdsFileName)
+    res[[i]] <- scei
+  }
+  sce <- do.call(SingleCellExperiment::cbind, res)
+  return(sce)
 }
   
   
@@ -109,7 +132,14 @@ importDropEst <- function(dropEstdirs = NULL,
 #library("SingleCellExperiment")
 #library("SummarizedExperiment")
 #library("tidyverse")
-#sce <- importDropEst(dropEstdirs = ../inst/extdata/DropEst_neurons_900_10x/', counts_matrix_type='filtered')
+#smplpath <- '/Users/sbandya/Desktop/dropest_samples/'
+#smplnames <- c('sample1','sample2','sample3')
+#smplnames <- c('sample1')
+#smpldirs <- as.list(paste0(smplpath,smplnames))
+#print(smpldirs)
+#sce <- importDropEst(sampleDirs = smpldirs, sampleNames = smplnames , dataType='filtered')
 #print(names(metadata(sce)))
+#print(head(colData(sce)))
+
 
 
