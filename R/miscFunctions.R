1d68e1ba52d9e7f4645ffbd5ced5447e54da75bf
Shirene Cao <xcao19@bu.edu>
2020-03-07 09:35:17 -0500

    Added in example use of decorator and console logging feature

--- R/miscFunctions.R
+++ R/miscFunctions.R
@@ -71,7 +71,7 @@ summarizeTable <- function(inSCE, useAssay="counts", expressionCutoff=1700){
 #' newSCE <- createSCE(assayFile = counts_mat, annotFile = sample_annot,
 #'                     featureFile = row_annot, assayName = "counts",
 #'                     inputDataFrames = TRUE, createLogCounts = TRUE)
-createSCE <- function(assayFile=NULL, annotFile=NULL, featureFile=NULL,
+createSCE <- simpleLog %@% function(assayFile=NULL, annotFile=NULL, featureFile=NULL,
                       assayName="counts", inputDataFrames=FALSE,
                       createLogCounts=TRUE){
   
