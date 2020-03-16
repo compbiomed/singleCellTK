971205426a9e29acf3c4fb71373fe9dee0e13978
salzcamino <salam.alabdullatif@gmail.com>
2020-03-16 14:02:45 -0400

    tabsetPanel test

--- inst/shiny/ui_08_2_cellviewer.R
+++ inst/shiny/ui_08_2_cellviewer.R
@@ -4,7 +4,8 @@ shinyPanelCellViewer <- fluidPage(
            h1("Cell Viewer"),
            fluidRow(
              column(3,
-                    wellPanel(style = "background: floralwhite",
+               tabsetPanel(
+                    tabPanel(style = "background: floralwhite",
                             # Section 1 - Assay Settings
                             actionButton("cv_button1", h4(strong("Select Coordinates")),style = "background: floralwhite"),
                             # open by default
@@ -137,10 +138,10 @@ shinyPanelCellViewer <- fluidPage(
                                 fluidRow(column(6,sliderInput("adjustalpha", h5(strong("Opacity:")), min = 0, max = 1, value = 1)),
                                          column(6,sliderInput("adjustsize", h5(strong("Size:")), min = 0.1, max = 0.8, value = 0.45)),
                                          column(6,textInput("adjustxlab", h5(strong("X-axis label:")))),
-                                         column(6,textInput("adjustylab", h5(strong("Y-axis label:")))),
-                                         column(6, checkboxInput("checkbinning",h5("Perform Binning:"), value = FALSE))
+                                         column(6,textInput("adjustylab", h5(strong("Y-axis label:"))))
                                   )
              ))
+             )
            )#fluidrow_end
            # )#well_end
   )#tag_end
