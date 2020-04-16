398ecaa3d8e2335a8b75ebd4b8e0023391213459
Shruthi Bandyadka <shruthibhat93@gmail.com>
2020-04-16 13:23:37 -0400

    Updated exportSCEtoAdata: changed param names, vectorized compression input params

--- R/exportSCEtoAnndata.R
+++ R/exportSCEtoAnndata.R
@@ -1,20 +1,20 @@
 #' @title Export a \link[SingleCellExperiment]{SingleCellExperiment} R object as 
-#' Python Anndata object 
+#' Python annData object 
 #' @description Writes all assays, colData, rowData, reducedDims, and altExps objects in a
-#' \link[SingleCellExperiment]{SingleCellExperiment} to a Python Anndata object in the .h5ad format
+#' \link[SingleCellExperiment]{SingleCellExperiment} to a Python annData object in the .h5ad format
 #' All parameters of Anndata.write_h5ad function (https://icb-anndata.readthedocs-hosted.com/en/stable/anndata.AnnData.write_h5ad.html)
 #' are available as parameters to this export function and set to defaults. Defaults can be
 #' overridden at function call.
 #' @param sce \link[SingleCellExperiment]{SingleCellExperiment} R object to be
 #'  exported.
+#'  @param useAssay Character, default `"counts"`. The name of assay of
+#' interests that will be set as the primary matrix of the output AnnData.
 #' @param outputDir Path to the directory where .h5ad outputs will be written
 #' @param overwrite Boolean. Default \code{TRUE}.
 #' @param compression Default \code{NULL}.If output file compression is required, this variable accepts
 #' 'gzip' or 'lzf' as inputs.
-#' #' @param compression_opts Integer. Default \code{NULL} Sets the compression level
-#' @param as_dense Default \code{NULL} Sparse arrays in AnnData object to write as dense. 
-#' Refer anndata.write_h5ad documentation for details
-#' @param force_dense Default \code{NULL} Write sparse data as a dense matrix.
+#' @param compression_opts Integer. Default \code{NULL} Sets the compression level
+#' @param forceDense Default \code{NULL} Write sparse data as a dense matrix.
 #' Refer anndata.write_h5ad documentation for details
 #' @examples
 #' data(sce_chcl, package = "scds")
@@ -23,14 +23,19 @@
 #' @export
 
 exportSCEtoAnnData <- function(sce, 
-                                mainAssay='counts',
+                                useAssay='counts',
                                 outputDir="./",
                                 sample = "sample",
                                 overwrite=TRUE,
-                                compression= NULL,
-                                compression_opts = NULL,
-                                as_dense="()",
-                                force_dense= NULL){
+                                compression= c('gzip','lzf','None'),
+                                compressionOpts = NULL,
+                                forceDense= c('False','True')){
+  compression <- match.arg(compression)
+  forceDense <- match.arg(forceDense)
+  if (compression == 'None'){
+    compression <- NULL
+  }
+
   if (!reticulate::py_module_available(module = "scanpy")) {
     warning("Cannot find python module 'scanpy', please install Conda and",
             " run sctkPythonInstallConda() or run sctkPythonInstallVirtualEnv().",
@@ -42,18 +47,17 @@ exportSCEtoAnnData <- function(sce,
             " function from the 'reticulate' package can be used to select the",
             " correct Python environment.")
     return(sce)}
-  #sc <- reticulate::import("scanpy")
-  anndata <- .sce2adata(sce)
-  filename <- paste0(sample,".h5ad")
-  filepath <- file.path(outputDir,filename)
   
-  if (file.exists(filepath) && !isTRUE(overwrite)) {
+  annData <- .sce2adata(sce,useAssay)
+  fileName <- paste0(sample,".h5ad")
+  filePath <- file.path(outputDir,fileName)
+  
+  if (file.exists(filePath) && !isTRUE(overwrite)) {
     stop(paste0(path, " already exists. Change 'outputDir' or set 'overwrite' to TRUE."))
     }
-  
-  anndata$write_h5ad(filepath,
+
+  annData$write_h5ad(filePath,
                      compression = compression, 
-                     compression_opts = compression_opts,
-                     as_dense=as_dense,
-                     force_dense = force_dense)
+                     compression_opts = compressionOpts,
+                     force_dense = forceDense)
 }
