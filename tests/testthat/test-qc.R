18f7fd42c16b24721f1e7a6bd6d7e8bfad607260
Shruthi Bandyadka <shruthibhat93@gmail.com>
2020-04-29 14:37:31 -0400

    test QC functions - add scran

--- tests/testthat/test-qc.R
+++ tests/testthat/test-qc.R
@@ -7,21 +7,29 @@ data(sce_chcl, package = "scds")
 context("Testing QC functions")
 
 test_that("Testing runScrublet",{
-  sce_testscrublet <- runScrublet(sce_chcl)
-  expect_equal(class(colData(sce_testscrublet)$scrublet_score), 'numeric')
-  expect_equal(class(colData(sce_testscrublet)$scrublet_call), 'logical')
-  expect_equal(dim(reducedDim(sce_testscrublet,'TSNE')), c(dim(sce_testscrublet)[1],2))
-  expect_equal(dim(reducedDim(sce_testscrublet,'UMAP')), c(dim(sce_testscrublet)[1],2))
+  sce <- runScrublet(sce_chcl)
+  expect_equal(class(colData(sce)$scrublet_score), 'numeric')
+  expect_equal(class(colData(sce)$scrublet_call), 'logical')
+  expect_equal(dim(reducedDim(sce,'TSNE')), c(dim(sce)[1],2))
+  expect_equal(dim(reducedDim(sce,'UMAP')), c(dim(sce)[1],2))
+})
+
+test_that("Testing runCxdsBcdsHybrid",{
+  sce <- runCxdsBcdsHybrid(sce_chcl)
+  expect_equal(class(colData(sce)$scds_hybrid_score), 'numeric')
+  expect_equal(class(colData(sce)$doublet_true_labels), 'character')
 })
 
 test_that(desc = "Testing runDoubletCells", {
-  sceres <- runDoubletCells(sce)
-  expect_equal(length(colData(sceres)$scran_doubletCells_Score),ncol(sce))
-  expect_equal(class(colData(sceres)$scran_doubletCells_Score), "numeric")
+  sce <- runDoubletCells(sce_chcl)
+  expect_equal(length(colData(sce)$scran_doubletCells_Score),ncol(sce_chcl))
+  expect_equal(class(colData(sce)$scran_doubletCells_Score), "numeric")
 })
 
 test_that(desc = "Testing runDoubletFinder",  {
-  sceres <- runDoubletFinder(sce, seuratPcs = 1:3, seuratNfeatures = 100, seuratRes = 1)
-  expect_equal(length(colData(sceres)$doubletFinder_doublet_score_Resolution_1),ncol(sce))
-  expect_equal(class(colData(sceres)$doubletFinder_doublet_score_Resolution_1), "numeric")
+  sce <- runDoubletFinder(sce_chcl, seuratPcs = 1:3, seuratNfeatures = 100, seuratRes = 1)
+  expect_equal(length(colData(sce)$doubletFinder_doublet_score_Resolution_1),ncol(sce_chcl))
+  expect_equal(class(colData(sce)$doubletFinder_doublet_score_Resolution_1), "numeric")
 })
+
+
