e268fd483ff04946cadba3a670a77025849ce605
Shruthi Bandyadka <shruthibhat93@gmail.com>
2020-04-29 14:52:27 -0400

    test QC functions - add empty drops

--- tests/testthat/test-qc.R
+++ tests/testthat/test-qc.R
@@ -6,7 +6,7 @@ data(sce_chcl, package = "scds")
 
 context("Testing QC functions")
 
-test_that("Testing runScrublet",{
+test_that("Testing scrublet",{
   sce <- runScrublet(sce_chcl)
   expect_equal(class(colData(sce)$scrublet_score), 'numeric')
   expect_equal(class(colData(sce)$scrublet_call), 'logical')
@@ -14,19 +14,30 @@ test_that("Testing runScrublet",{
   expect_equal(dim(reducedDim(sce,'UMAP')), c(dim(sce)[1],2))
 })
 
-test_that("Testing runCxdsBcdsHybrid",{
+test_that("Testing scds",{
   sce <- runCxdsBcdsHybrid(sce_chcl)
   expect_equal(class(colData(sce)$scds_hybrid_score), 'numeric')
   expect_equal(class(colData(sce)$doublet_true_labels), 'character')
 })
 
-test_that(desc = "Testing runDoubletCells", {
+test_that("Testing emptydrops",{
+  sce <- runEmptyDrops(sce_chcl)
+  expect_equal(class(colData(sce)$doublet_true_labels), 'character')
+  expect_equal(class(colData(sce)$dropletUtils_emptyDrops_total), 'integer')
+  expect_equal(class(colData(sce)$dropletUtils_emptyDrops_logprob), 'numeric')
+  expect_equal(class(colData(sce)$dropletUtils_emptyDrops_pvalue), 'numeric')
+  expect_equal(class(colData(sce)$dropletUtils_emptyDrops_limited), 'logical')
+  expect_equal(class(colData(sce)$dropletUtils_emptyDrops_fdr), 'numeric')
+})
+
+
+test_that(desc = "Testing scran", {
   sce <- runDoubletCells(sce_chcl)
   expect_equal(length(colData(sce)$scran_doubletCells_Score),ncol(sce_chcl))
   expect_equal(class(colData(sce)$scran_doubletCells_Score), "numeric")
 })
 
-test_that(desc = "Testing runDoubletFinder",  {
+test_that(desc = "Testing DoubletFinder",  {
   sce <- runDoubletFinder(sce_chcl, seuratPcs = 1:3, seuratNfeatures = 100, seuratRes = 1)
   expect_equal(length(colData(sce)$doubletFinder_doublet_score_Resolution_1),ncol(sce_chcl))
   expect_equal(class(colData(sce)$doubletFinder_doublet_score_Resolution_1), "numeric")
