db48b106c65d9576584624b30f32bc5ce1a3c582
Yusuke Koga <ykoga07@bu.edu>
2020-05-13 10:43:19 -0400

    Change decontX test unit to using mouse dataset

--- tests/testthat/test-decontX.R
+++ tests/testthat/test-decontX.R
@@ -1,9 +1,9 @@
 # decontamination algorithms
 library(singleCellTK)
 context("Testing decontamination algorithms")
-sce <- emptyDropsSceExample
+sce <- mouseBrainSubsetSCE
 
-test_that(desc = "Testing runDoubletCells", {
+test_that(desc = "Testing runDecontX", {
         sceres <- runDecontX(sce)
         expect_equal(length(colData(sceres)$decontX_clusters),ncol(sce))
         expect_equal(class(colData(sceres)$decontX_contamination), "numeric")
