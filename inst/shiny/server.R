4ff80358fbdfef9ae3692e81f2f6030a518fc77f
Salam AlAbdullatif <salamha@bu.edu>
2020-06-09 18:39:59 -0400

    test plots

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -2904,7 +2904,7 @@ shinyServer(function(input, output, session) {
     if(input$viewertabs == "reducedDims Plot"){
       if(input$TypeSelect_Colorby == "Pick a Color"){
         a <- plotSCEDimReduceColData(vals$counts, reducedDimName = input$QuickAccess,
-          xlab = xname, ylab = yname, legendTitle = legendname, title = input$adjusttitle)
+          xlab = xname, ylab = yname, title = input$adjusttitle)
         ggplotly(a, tooltip = c("X_input", "Y_input"), height = 600)
       }else if(input$TypeSelect_Colorby == "Expression Assays"){
         a <- plotSCEDimReduceFeatures(vals$counts, feature = input$GeneSelect_Assays_Colorby,
@@ -2913,7 +2913,8 @@ shinyServer(function(input, output, session) {
         ggplotly(a, tooltip = c("X_input", "Y_input", "Color"), height = 600)
       }else if(input$viewertabs == "Cell Annotation"){
         a <- plotSCEDimReduceColData(vals$counts, reducedDimName = input$QuickAccess,
-          xlab = xname, ylab = yname, legendTitle = legendname, title = input$adjusttitle)
+          xlab = xname, ylab = yname, legendTitle = legendname, title = input$adjusttitle,
+          colorBy = input$AnnotationSelect_Colorby)
         ggplotly(a, tooltip = c("X_input", "Y_input", "Color"), height = 600)
       }
     }else if(input$viewertabs == "Bar Plot"){
