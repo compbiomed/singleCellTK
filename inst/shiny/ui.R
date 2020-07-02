cbaa986872ec1533437fba32a78209e5cae3d7c5
irzamsarfraz <irzam9095@gmail.com>
2020-07-02 23:13:42 +0500

    Temporary change. Remove these.

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -36,8 +36,23 @@ library(broom)
 library(RColorBrewer)
 library(grDevices)
 
+#remove these below
+library(shiny)
+library("rvest")
+library(stringr)
+library(stringi)
+library(readtext)
+library(DT)
+
 source("helpers.R")
 source("colourGroupInput.R")
+source("../../R/seuratFunctions.R")
+source("../../R/computeZScore.R")
+source("../../R/trimCounts.R")
+source("../../R/scran_modelGeneVar.R")
+source("../../R/getTopHVG.R")
+source("../../R/scater_logNormCounts.R")
+source("../../R/miscFunctions.R")
 data("c2BroadSets")
 
 #test internet connection for enrichR connectivity
