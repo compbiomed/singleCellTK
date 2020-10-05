a2d0c4ce2879defcb062d4fdbc13c6f587552d2d
Yichen Wang <wangych@bu.edu>
2020-10-05 17:14:44 -0400

    add differential expression report

--- R/htmlReports.R
+++ R/htmlReports.R
@@ -1,9 +1,9 @@
-#' @title Get runDropletQC .html report 
+#' @title Get runDropletQC .html report
 #' @description A  function to generate .html Rmarkdown report containing the visualizations of the runDropletQC function output
 #' @param inSCE A \link[SingleCellExperiment]{SingleCellExperiment} object containing
 #' the full droplet count matrix with the output from runDropletQC function
-#' @param study description of the data set and experiment design. Default is NULL. 
-#' @param output_file name of the generated file. If NULL/default then the output file name will be based on the name of the Rmarkdown template 
+#' @param study description of the data set and experiment design. Default is NULL.
+#' @param output_file name of the generated file. If NULL/default then the output file name will be based on the name of the Rmarkdown template
 #' @param output_dir name of the output directory to save the rendered file. If NULL/default the file is stored to the current working directory
 #' @return .html file
 #' @examples
@@ -16,23 +16,23 @@
 reportDropletQC <- function(inSCE, output_file = NULL,
                                    output_dir = NULL,
                                    study = NULL) {
-  
+
   if (is.null(output_dir)){
     output_dir<- getwd()
     }
- 
-  rmarkdown::render(system.file("rmarkdown/qc/DropletQC.Rmd", package="singleCellTK"), 
-    params = list(object=inSCE, study=study), output_file = output_file, 
+
+  rmarkdown::render(system.file("rmarkdown/qc/DropletQC.Rmd", package="singleCellTK"),
+    params = list(object=inSCE, study=study), output_file = output_file,
     output_dir = output_dir )
  }
 
 
-#' @title Get runCellQC .html report 
+#' @title Get runCellQC .html report
 #' @description A  function to generate .html Rmarkdown report containing the visualizations of the runCellQC function output
 #' @param inSCE A \link[SingleCellExperiment]{SingleCellExperiment} object containing
 #' the filtered count matrix with the output from runCellQC function
-#' @param study description of the data set and experiment design. Default is NULL. 
-#' @param output_file name of the generated file. If NULL/default then the output file name will be based on the name of the Rmarkdown template. 
+#' @param study description of the data set and experiment design. Default is NULL.
+#' @param output_file name of the generated file. If NULL/default then the output file name will be based on the name of the Rmarkdown template.
 #' @param output_dir name of the output directory to save the rendered file. If NULL/default the file is stored to the current working directory
 #' @return .html file
 #' @examples
@@ -49,8 +49,8 @@ reportCellQC <- function(inSCE, output_file = NULL,
   if (is.null(output_dir)){
     output_dir<- getwd()
   }
-  rmarkdown::render(system.file("rmarkdown/qc/CellQC.Rmd", package="singleCellTK"), 
-    params = list(object=inSCE, study=study), output_file = output_file, 
+  rmarkdown::render(system.file("rmarkdown/qc/CellQC.Rmd", package="singleCellTK"),
+    params = list(object=inSCE, study=study), output_file = output_file,
     output_dir = output_dir)
 }
 
@@ -62,7 +62,7 @@ reportCellQC <- function(inSCE, output_file = NULL,
 #' runQCMetrics, runScrublet, runDoubletCells, runCxds, runBcds, runCxdsBcdsHybrid, runDecontX, runBarcodeRankDrops, runEmptyDrops
 #' @param algorithm Character. Specifies which QC algorithm report to generate.
 #'  Available options are "BarcodeRankDrops", "EmptyDrops", "QCMetrics", "Scrublet", "DoubletCells", "Cxds", "Bcds", "CxdsBcdsHybrid", "DoubletFinder"  and "DecontX".
-#' @param output_file name of the generated file. If NULL/default then the output file name will be based on the name of the selected QC algorithm name . 
+#' @param output_file name of the generated file. If NULL/default then the output file name will be based on the name of the selected QC algorithm name .
 #' @param output_dir name of the output directory to save the rendered file. If NULL/default the file is stored to the current working directory
 #' @return .html file
 #' @examples
@@ -85,13 +85,13 @@ reportQCTool <- function(inSCE, algorithm=c("BarcodeRankDrops",
                                             "DecontX"),
                          output_file = NULL,
                             output_dir = NULL) {
-  
+
   algorithm <- match.arg(algorithm)
-  
+
   if (is.null(output_dir)){
     output_dir<- getwd()
   }
-  
+
   if (algorithm =="BarcodeRankDrops"){
     rmarkdown::render(system.file("rmarkdown/qc/BarcodeRankDrops.Rmd", package="singleCellTK"), params = list(
       object=inSCE), output_file = output_file, output_dir = output_dir)
@@ -135,4 +135,34 @@ reportQCTool <- function(inSCE, algorithm=c("BarcodeRankDrops",
  }
 
 
-    
+#' @title Get runDEAnalysis .html report
+#' @description A  function to generate .html Rmarkdown report containing the
+#' visualizations of the \code{\link{runDEAnalysis}} function output
+#' @param inSCE A \code{\link[SingleCellExperiment]{SingleCellExperiment}}
+#' object containing the output from \code{\link{runDEAnalysis}} function
+#' @param study The specific analysis to visualize, used as \code{analysisName}
+#' argument when running differential expression.
+#' @param output_file name of the generated file. If \code{NULL} then the output
+#' file name will be based on the name of the Rmarkdown template. Default
+#' \code{NULL}.
+#' @param output_dir name of the output directory to save the rendered file. If
+#' \code{NULL} the file is stored to the current working directory.
+#' Default \code{NULL}.
+#' @return .html file
+#' @export
+reportDiffExp <- function(inSCE, study,
+                          output_file = NULL,
+                          output_dir = NULL) {
+
+  if (is.null(output_dir)){
+    output_dir <- getwd()
+  }
+  if (!study %in% names(metadata(inSCE)$diffExp)) {
+    stop("Specified study not found in given SCE object")
+  }
+  rmarkdown::render(system.file("rmarkdown/de/DifferentialExpression.Rmd",
+                                package="singleCellTK"),
+                    params = list(object=inSCE, study=study),
+                    output_file = output_file,
+                    output_dir = output_dir )
+}
