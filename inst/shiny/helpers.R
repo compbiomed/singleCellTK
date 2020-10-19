39ab9960bdb11a948f35cc7991be0e374fee1b0c
Vidya Akavoor <vidyaap@bu.edu>
2020-10-19 15:32:06 -0400

    Tweak single-column QC plot layout

--- inst/shiny/helpers.R
+++ inst/shiny/helpers.R
@@ -207,70 +207,56 @@ formatGeneSetDBChoices <- function(dbIDs, dbCats) {
 #--------------#
 # QC/Filtering #
 #--------------#
-
-# combineQCSubPlots <- function(output, combineP, algo, sampleList, plots, plotIds, statuses) {
-#   if (combineP=="all") {
-#     output[[plotIds[[algo]]]] <- renderPlot(plots, height = 800)
-#   } else {
-#     tabsetID <- paste0(algo, "Tabs") # for the tabsetPanel within a tab
-#     removeUI(selector = paste0("#", plotIds[[algo]]))
-# 
-#     for (i in seq_along(sampleList)) {
-#       s <- sampleList[[i]]
-#       sID <- paste(c(algo, s, "Tab"), collapse = "")
-#       subPlotID <- paste(c(algo, s, "Plot"), collapse = "")
-#       if (is.null(statuses[[algo]][[s]])) {
-#         if (i == 1) {
-#           appendTab(tabsetID, tabPanel(s, fluidPage(id = sID, plotOutput(outputId = subPlotID))), select = TRUE)
-#         } else {
-#           appendTab(tabsetID, tabPanel(s, fluidPage(id = sID, plotOutput(outputId = subPlotID))), select = FALSE)
-#         }
-#       }
-#       output[[subPlotID]] <- renderPlot(plots$Sample[[s]])
-#     }
-#   }
-# }
-
-
-# combineQCSubPlots <- function(output, combineP, algo, sampleList, plots, plotIds, statuses) {
-#   # idsToPlots <- list()
-#   if (combineP=="all") {
-#     output[[plotIds[[algo]]]] <- renderPlot(plots, height = 800)
-#   } else {
-#     tabsetID <- paste0(algo, "Tabs") # for the tabsetPanel within a tab
-#     output[[plotIds[[algo]]]] <- renderPlot(plots$Violin)
-#     
-#     for (i in seq_along(sampleList)) {
-#       local({
-#         s <- sampleList[[i]]
-#         sID <- paste(c(algo, s, "Tab"), collapse = "")
-#         if (is.null(statuses[[algo]][[s]])) {
-#           if (i == 1) {
-#             appendTab(tabsetID, tabPanel(s, fluidPage(id = sID)), select = TRUE)
-#           } else {
-#             appendTab(tabsetID, tabPanel(s, fluidPage(id = sID)), select = FALSE)
-#           }
-#           
-#         }
-#         
-#         for (subScore in names(plots$Sample[[s]])) {
-#           local({
-#             subPlotID <- paste(c(algo, s, subScore), collapse="")
-#             insertUI(
-#               selector = paste0("#", sID),
-#               ui = plotOutput(outputId = subPlotID)
-#             )
-#             # print(subPlotID)
-#             # print(subScore)
-#             # idsToPlots[[subPlotID]] <- plots$Sample[[s]][[subScore]]
-#             output[[subPlotID]] <- renderPlot(plots$Sample[[s]][[subScore]])
-#           })
-#         }
-#       })
-#     }
-#   }
-#   # return(idsToPlots)
-# }
+combineQCMPlots <- function(output, combineP, sampleList, plots, plotIds) {
+  if (length(sampleList) == 1) {
+    # Plot output code from https://gist.github.com/wch/5436415/
+    output[[plotIds$QCMetrics]] <- renderUI({
+      plot_output_list <- lapply(names(plots), function(subScore) {
+        subPlotID <- paste0("QCMetrics", subScore)
+        plotOutput(subPlotID)
+      })
+      
+      # Convert the list to a tagList - this is necessary for the list of items
+      # to display properly.
+      do.call(tagList, plot_output_list)
+    })
+    
+    for (subScore in names(plots)) {
+      # Need local so that each item gets its own number. Without it, the value
+      # of i in the renderPlot() will be the same across all instances, because
+      # of when the expression is evaluated.
+      local({
+        my_subScore <- subScore
+        subPlotID <- paste0("QCMetrics", subScore)
+        output[[subPlotID]] <- renderPlot(plots[[my_subScore]])
+      })
+    }
+  } else {
+    
+    output[[plotIds$QCMetrics]] <- renderUI({
+      plot_output_list <- lapply(names(plots$Violin), function(subScore) {
+        subPlotID <- paste0("QCMetrics", subScore)
+        plotOutput(subPlotID)
+      })
+      
+      # Convert the list to a tagList - this is necessary for the list of items
+      # to display properly.
+      do.call(tagList, plot_output_list)
+    })
+    
+    for (subScore in names(plots$Violin)) {
+      # Need local so that each item gets its own number. Without it, the value
+      # of i in the renderPlot() will be the same across all instances, because
+      # of when the expression is evaluated.
+      local({
+        my_subScore <- subScore
+        subPlotID <- paste0("QCMetrics", my_subScore)
+        
+        output[[subPlotID]] <- renderPlot(plots$Violin[[my_subScore]])
+      })
+    }
+  }
+}
 
 combineQCSubPlots <- function(output, combineP, algo, sampleList, plots, plotIds, statuses) {
   if (length(sampleList) == 1) {
@@ -300,7 +286,6 @@ combineQCSubPlots <- function(output, combineP, algo, sampleList, plots, plotIds
     tabsetID <- paste0(algo, "Tabs") # for the tabsetPanel within a tab
     mainPlotID <- paste0(plotIds[[algo]], "Main")
     output[[plotIds[[algo]]]] <- renderUI(plotOutput(mainPlotID))
-    
     output[[mainPlotID]] <- renderPlot(plots$Violin)
     
     for (i in seq_along(sampleList)) {
@@ -341,17 +326,12 @@ combineQCSubPlots <- function(output, combineP, algo, sampleList, plots, plotIds
       })
     }
   }
-  # return(idsToPlots)
 }
 
 
 arrangeQCPlots <- function(inSCE, output, algoList, sampleList, plotIDs, statuses, redDimName) {
   uniqueSampleNames <- unique(sampleList)
   combineP <- "none"
-  # combineP <- "all"
-  # if (length(uniqueSampleNames) > 1) {
-  #   combineP <- "sample"
-  # }
   for (a in algoList) {
     if (a == "doubletCells") {
       dcPlots <- plotDoubletCellsResults(inSCE, combinePlot = combineP, sample = sampleList, 
@@ -375,7 +355,7 @@ arrangeQCPlots <- function(inSCE, output, algoList, sampleList, plotIDs, statuse
       combineQCSubPlots(output, combineP, a, uniqueSampleNames, dxPlots, plotIDs, statuses)
     } else if (a == "QCMetrics") {
       qcmPlots <- plotRunPerCellQCResults(inSCE, sample = sampleList, combinePlot = combineP, plotLabels = "none")
-      combineQCSubPlots(output, combineP, a, uniqueSampleNames, qcmPlots, plotIDs, statuses)
+      combineQCMPlots(output, combineP, uniqueSampleNames, qcmPlots, plotIDs)
       
     } else if (a == "scrublet") {
       sPlots <- plotScrubletResults(inSCE, combinePlot = combineP, sample = sampleList, 
