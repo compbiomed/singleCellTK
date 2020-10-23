e5c08a29c448488dcd4f3019a87c8ff190b60398
rz2333 <ruihong2333@gmail.com>
2020-10-23 10:19:55 -0400

    Fix samplesize calculation: Number of cells

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -5353,7 +5353,7 @@ shinyServer(function(input, output, session) {
     } else{
       withBusyIndicatorServer("runSubsampleCells", {
         if (input$useReadCount){
-          vals$subCells <- DownsampleCells(originalData = vals$counts,
+          vals$subCells <- downSampleCells(originalData = vals$counts,
                                            useAssay = input$cellsAssay,
                                            realLabels = input$selectCellNumCondition,
                                            totalReads = sum(SummarizedExperiment::assay(vals$counts, input$cellsAssay)),
@@ -5365,7 +5365,7 @@ shinyServer(function(input, output, session) {
                                            iterations = input$iterations)
         }
         else{
-          vals$subCells <- DownsampleCells(originalData = vals$counts,
+          vals$subCells <- downSampleCells(originalData = vals$counts,
                                            useAssay = input$cellsAssay,
                                            realLabels = input$selectCellNumCondition,
                                            totalReads = input$totalReads,
