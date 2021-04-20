8795635e578e69df4c08b1f92c823d90be8512d3
Irzam Sarfraz <irzam9095@gmail.com>
2021-04-20 23:32:30 +0500

    Some fixes to seuratReport

--- R/htmlReports.R
+++ R/htmlReports.R
@@ -183,3 +183,78 @@ reportDiffExp <- function(inSCE, study,
                     output_file = output_file,
                     output_dir = output_dir )
 }
+
+#' Computes an HTML report from the Seurat workflow and returns the output SCE 
+#'  object with the computations stored in it.
+#' @param inSCE Input \code{SingleCellExperiment} object.
+#' @param outputFile Specify the name of the generated output HTML file. If \code{NULL} then the output
+#' file name will be based on the name of the Rmarkdown template. Default
+#' \code{NULL}.
+#' @param outputDir Specify the name of the output directory to save the 
+#'  rendered HTML file. If \code{NULL} the file is stored to the current 
+#'  working directory.
+#' @param subtitle A \code{character} value specifying the subtitle to use in the
+#'  Seurat report.
+#' @param authors A \code{character} value specifying the names of the authors 
+#'  to use in the Seurat report.
+#' @param sce A \code{character} value specifying the path of the input
+#'  \code{SingleCellExperiment} object.
+#' @param biological.group A character value that specifies the name of the
+#'  \code{colData} column to use as the main biological group in the seurat
+#'  report for differential expression and grouping.
+#' @param phenotype.groups A \code{character} vector that specifies the names
+#'  of the \code{colData} columns to use for differential expression in addition
+#'  to the \code{biological.group} parameter.
+#' @param selected.markers A \code{character} vector specifying the user decided
+#'  gene symbols of pre-selected markers that be used to generate gene plots in 
+#'  addition to the gene markers computed from differential expression.
+#' @param clustering.resolution A \code{numeric} value indicating the resolution
+#'  to use with clustering. Default is \code{0.8}.
+#' @param variable.features A \code{numeric} value indicating the number of
+#'  top variable genes to identify in the seurat report. Default is \code{2000}.
+#' @param pc.count A \code{numeric} value indicating the number of principal
+#'  components to use in the analysis workflow. Default is \code{10}.
+#'
+#' @return A \code{SingleCellExperiment} object that has the seurat computations
+#'  stored and can be used to interactively visualize the plots by importing
+#'  in the \code{singleCellTK} user interface.
+#' @export
+seuratReport <- function(inSCE,
+                         outputFile = NULL,
+                         outputDir = NULL,
+                         subtitle = "BUMC Single Cell Sequencing Core",
+                         authors = "Tianmu (Timo) Hu, Irzam Sarfraz",
+                         sce = NULL,
+                         biological.group = NULL,
+                         phenotype.groups = NULL,
+                         selected.markers = NULL,
+                         clustering.resolution = 0.8,
+                         variable.features = 2000,
+                         pc.count = 10){
+  
+  rmarkdown::render(system.file("rmarkdown/seurat/SeuratReport.Rmd",
+                                package="singleCellTK"),
+                    params = list(
+                      subtitle = subtitle,
+                      authors = authors,
+                      sce = inSCE,
+                      biological.group = biological.group,
+                      phenotype.groups = phenotype.groups,
+                      selected.markers = selected.markers,
+                      clustering.resolution = clustering.resolution,
+                      variable.features = variable.features,
+                      pc.count = pc.count,
+                      outputPath = outputDir
+                    ),
+                    output_file = outputFile,
+                    output_dir = outputDir)
+  
+  path <- paste0(outputDir, "SCE_SeuratReport", "-", gsub(" ", "_", Sys.Date()), ".rds")
+  outSCE <- readRDS(path)
+  
+  message("Output SCE object stored as ", paste0("SCE_SeuratReport", "-", gsub(" ", "_", Sys.Date()), ".rds"), " in ", outputDir, ".")
+  message("Output HTML file stored as ", outputFile, " in ", outputDir, ".")
+  
+  return(outSCE)
+}
+
