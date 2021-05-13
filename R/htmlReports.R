35ddfc2abb01c6ea3c57538fc1948f9a676c3437
Salam AlAbdullatif <salamha@bu.edu>
2021-05-13 14:08:20 -0400

    remove extra reports

--- R/htmlReports.R
+++ R/htmlReports.R
@@ -321,3 +321,53 @@ seuratReport <- function(inSCE,
   return(outSCE)
 }
 
+
+#' @export
+reportDifferentialFET <-
+    function(sce,
+             variable,
+             phenotype,
+             control,
+             case,
+             output_dir = ".",
+             output_file = "DifferentialAbundanceFET_Report",
+             pdf = FALSE,
+             showSession = TRUE
+    ){
+        res <- differentialAbundanceFET(sce, variable, phenotype, control, case)
+        rmarkdown::render(
+            system.file("rmarkdown/DifferentialAbundanceFET_Report.Rmd"),
+            params = list(
+                sce = sce,
+                res = res,
+                pdf = isTRUE(pdf),
+                showSession = isTRUE(showSession)
+            ),
+            output_file = output_file,
+            output_dir = output_dir
+        )
+    }
+
+
+#' @export
+reportClusterAbundance <- function(sce,
+                                   variable,
+                                   phenotype,
+                                   output_dir = ".",
+                                   output_file = "plotClusterAbundance_Report",
+                                   pdf = FALSE,
+                                   showSession = TRUE
+){
+    plot <- plotClusterAbundance(sce, variable, phenotype)
+    rmarkdown::render(
+        system.file("rmarkdown/PlotClusterAbundance_Report.Rmd"),
+        params = list(
+            sce = sce,
+            plot = plot,
+            pdf = isTRUE(pdf),
+            showSession = isTRUE(showSession)
+        ),
+        output_file = output_file,
+        output_dir = output_dir
+    )
+}
