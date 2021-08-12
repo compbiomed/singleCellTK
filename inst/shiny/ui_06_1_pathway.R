375f8b4b4e2c34dc2e1c5c1b56c31f017341c9af
AFINITI\unais.qayyum <unais.qayyum@afiniti.com>
2021-08-12 18:34:02 +0500

    ui for vam and gsva

--- inst/shiny/ui_06_1_pathway.R
+++ inst/shiny/ui_06_1_pathway.R
@@ -1,35 +1,92 @@
-shinyPanelPathway <- fluidPage(
+shinyPanelvam <- fluidPage(
   tags$div(
     class = "container",
-    h1("Pathway Activity Analysis"),
-    h5(tags$a(href = paste0(docs.artPath, "ui_gsva.html"),
-              "(help)", target = "_blank")),
+    h1("Pathway Analysis"),
+    #h5(tags$a(href = paste0(docs.artPath, "ui_gsva.html"),
+     #         "(help)", target = "_blank")),
     sidebarLayout(
       sidebarPanel(
-        uiOutput("pathwayAssay"),
-        #selectInput("pathwayAssay", "Select Assay:", currassays),
-        selectInput("pathwayMethod", "Select Method:", "GSVA"),
-        uiOutput("selectPathwayGeneLists"),
-        uiOutput("selectNumTopPaths"),
-        selectInput("pathwayPlotVar",
-                    "Select Condition(s) of interest for plot:", clusterChoice,
-                    multiple = TRUE),
-        radioButtons("pathwayOutPlot", "Plot Type:", c("Heatmap", "Violin")),
+        uiOutput("vamAssay"),
+        selectInput(
+                    inputId = "pathway",
+                    label = "Select Method: ",
+                    choices = c(
+                      "VAM" = "VAM",
+                      "GSVA" = "GSVA"
+                    )  
+        ),
+        
+       uiOutput("selectPathwayGeneLists"),
+       
+       conditionalPanel(
+         condition = "input.pathway == 'VAM'",
+         selectInput(
+           inputId = "vamCenterParameter",
+           label = "Select Center Value: ",
+           choices = c(
+             "TRUE" = "TRUE",
+             "FALSE" = "FALSE"
+           )),
+         
+         selectInput(
+           inputId = "vamGammaParameter",
+           label = "Select Gamma Value: ",
+           choices = c(
+             "TRUE" = "TRUE",
+             "FALSE" = "FALSE"
+           )),
+       ),
+        
+       conditionalPanel(
+         condition = "input.pathway == 'GSVA'",
+         #radioButtons("pathwayOutPlot", "Plot Type:", c("Heatmap", "Violin"))
+         
+       ),
+       
         withBusyIndicatorUI(actionButton("pathwayRun", "Run")),
         tags$hr(),
-        h3("Save pathway results:"),
-        actionButton("savePathway", "Save Pathways"),
-        downloadButton("downloadPathway", "Download Pathway Results")
+        h3("Save results:"),
+        actionButton("savePathway", "Save Scores"),
+        downloadButton("downloadPathway", "Download Results")
       ),
       mainPanel(
         tabsetPanel(
           tabPanel(
             "Plot",
-            plotOutput("pathwayPlot", height = "600px")
+           
+            
+           radioButtons("Choice", label = NULL, c("Select Condition(s) of interest" = 'condition', 
+                                                  "Select Geneset of interest" = 'geneset'), 
+                                                  inline = TRUE),
+                
+            
+           
+           conditionalPanel(
+             condition = sprintf("input['%s'] == 'condition'", "Choice"),
+             selectInput("pathwayPlotVar",
+                         "Select Condition(s) of interest for plot:", clusterChoice,
+                         multiple = TRUE)
+           ),
+           
+           conditionalPanel(
+             condition = sprintf("input['%s'] == 'geneset'", "Choice"),
+             uiOutput("selectGeneSets")
+           ),
+           
+           plotOutput("pathwayPlot", height = "600px")
+           
           ),
+          
+          
+          
           tabPanel(
+            
             "Results Table",
-            DT::dataTableOutput("pathwaytable")
+             DT::dataTableOutput("pathwayTable"),
+             
+              
+            
+            
           )
         )
       )
