4c42e29d6937af042fc9a1e67d5bc05a59fdc4f1
Nida Pervaiz <nidapervaiz27@gmail.com>
2021-08-27 22:51:58 +0500

    changes in pathway analysis code snippet

--- inst/shiny/ui_06_1_pathwayAnalysis.R
+++ inst/shiny/ui_06_1_pathwayAnalysis.R
@@ -5,11 +5,6 @@ shinyPanelvam <- fluidPage(
     #h5(tags$a(href = paste0(docs.artPath, "ui_gsva.html"),
      #         "(help)", target = "_blank")),
     
-    
-    
-    
-    
-    
     sidebarLayout(
       sidebarPanel(
         uiOutput("vamAssay"),
@@ -55,11 +50,8 @@ shinyPanelvam <- fluidPage(
         dropdown(
           
           tags$h3("List of Input"),
-          
           uiOutput("selectReduceDim"),
-          
           uiOutput("selectGeneSets"),
-          
           selectInput(inputId = 'pathwayPlotVar',
                       label = 'Select Condition(s) of interest to group data (OPTIONAL) : ', 
                       choices = clusterChoice,
@@ -84,14 +76,13 @@ shinyPanelvam <- fluidPage(
           withBusyIndicatorUI(actionButton("Plot", "Update Plot")),  
           
           tags$style(".btn-custom {background-color: #e5e5e5; ;}"),
-          label = "Plot Options",
+          label = "Plot Options", 
           status = "custom", width = "300px", 
           animate = animateOptions(
             enter = animations$fading_entrances$fadeInLeftBig,
             exit = animations$fading_exits$fadeOutRightBig
           )
         ),
-        
         
         plotOutput("pathwayPlot", height = "600px") 
           
