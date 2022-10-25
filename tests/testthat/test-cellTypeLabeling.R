bc3b5e28b9f3fe2f3b96b91080c174d778e3cd4c
Yichen Wang <wangych@bu.edu>
2022-10-25 11:39:08 -0400

    Fix CHECK errors due to SingleR updates

--- tests/testthat/test-cellTypeLabeling.R
+++ tests/testthat/test-cellTypeLabeling.R
@@ -7,10 +7,17 @@ sce <- scaterlogNormCounts(sce, "logcounts")
 rownames(sce) <- rowData(sce)$feature_name
 
 test_that(desc = "Testing SingleR", {
-  sce <- runSingleR(sce)
-
-  testthat::expect_true("SingleR_hpca_main_pruned.labels" %in% names(colData(sce)))
-  testthat::expect_true("SingleR_hpca_main_labels" %in% names(colData(sce)))
-  testthat::expect_true("SingleR_hpca_main_first.labels" %in% names(colData(sce)))
-  testthat::expect_true("SingleR_hpca_main_scores" %in% names(colData(sce)))
+  tryCatch({
+    ref <- celldex::HumanPrimaryCellAtlasData()
+  }, error = function(e) {
+    message("Error importing reference with `celldex` library. ",
+            "Skipping runSingleR test.")
+  }, finally = {
+    sce <- runSingleR(sce)
+    testthat::expect_true("SingleR_hpca_main_scores" %in% names(colData(sce)))
+    testthat::expect_true("SingleR_hpca_main_labels" %in% names(colData(sce)))
+    #testthat::expect_true("SingleR_hpca_main_first.labels" %in% names(colData(sce)))
+    #testthat::expect_true("SingleR_hpca_main_delta.next" %in% names(colData(sce)))
+    testthat::expect_true("SingleR_hpca_main_pruned.labels" %in% names(colData(sce)))
+  })
 })
