5ff6fd1a4fc48ce96cf2a7abf42d3a8fbb2796e2
Akshay Aravind <aa2288@cornell.edu>
2023-09-24 14:58:53 -0400

    Pulled from origin devel to allow for successful merge

--- inst/shiny/ui_08_4_bubbleplot.R
+++ inst/shiny/ui_08_4_bubbleplot.R
@@ -5,73 +5,78 @@ shinyPanelBubbleplot <- fluidPage(
     p("Generic bubbleplot plotting panel for customized figure.",
       style = "color:grey;"),
     panel(
-      h3("Assay to Plot"),
+      
       fluidRow(
         column(width = 4,
+               hr(),
+               # Subset ####
+               h3("Cluster"),
+               uiOutput("bpClusterUI"),
+               h3("Feature"),
+               fluidRow(
+                 column(width = 12,
+                        selectizeInput(
+                          inputId = "bpAssay", 
+                          label = "Select input matrix:", 
+                          choices = NULL, 
+                          selected = NULL, 
+                          multiple = FALSE,
+                          options = NULL)
+                 )
+               ),
+               uiOutput('bpRowUI'),
                selectizeInput(
-                 inputId = "bpAssay", 
-                 label = "Select input matrix:", 
-                 choices = NULL, 
-                 selected = NULL, 
-                 multiple = FALSE,
-                 options = NULL)
-        )
-      ),
-      
-      hr(),
-      # Subset ####
-      h3("Cluster/Feature Subsetting"),
-      p("Select cluster and features of interests", style = "color:grey;"),
-      tabsetPanel(
-        tabPanel(
-          title = "Cluster",
-          uiOutput("bpClusterUI")),
-        tabPanel(
-          title = "Feature",
-          uiOutput('bpRowUI'),
-          selectizeInput(
-            'bpFeatures',
-            "Select Features",
-            choices = NULL, multiple = TRUE, width = '550px')
+                 'bpFeatures',
+                 "Select Features",
+                 choices = NULL, multiple = TRUE, width = '550px'),
+               withBusyIndicatorUI(actionButton("plotBubbleplot", "Plot Bubbleplot")),
+               hr(),
         ),
-      ),
-      hr(),
-      
-      # Others ####
-      h3("Bubbleplot Setting"),
-      p("Settings for title, label, color scheme and etc.",
-        style = "color:grey;"),
-      panel(
-        fluidRow(
-          column(width = 4,
-                 textInput("bpTitle", "Title", NULL)
-          )
-        ),
-        fluidRow(
-          column(width = 4,
-                 textInput("bpX", "X-axis Label", NULL)
-          ),
-          column(width = 4,
-                 textInput("bpY", "Y-axis Label", NULL)
-          )
-        ),
-        fluidRow(
-          column(
-            width = 4,
-            colourpicker::colourInput('bpLow', 'Low color', value = 'white')
-          ),
-          column(
-            width = 4,
-            colourpicker::colourInput('bpHigh', 'High color', value = 'blue')
+        
+        
+        
+        column(
+          8,
+          wellPanel(
+            h5(strong("Plotting Region")),
+            div(
+              style = 'height:800px;',
+              plotOutput("Bubbleplot")
+            ),
+            # Others ####
+            h3("Bubbleplot Setting"),
+            p("Settings for title, label, color scheme and etc.",
+              style = "color:grey;"),
+            panel(
+              fluidRow(
+                column(width = 4,
+                       textInput("bpTitle", "Title", NULL)
+                )
+              ),
+              fluidRow(
+                column(width = 4,
+                       textInput("bpX", "X-axis Label", NULL)
+                ),
+                column(width = 4,
+                       textInput("bpY", "Y-axis Label", NULL)
+                )
+              ),
+              fluidRow(
+                column(
+                  width = 4,
+                  colourpicker::colourInput('bpLow', 'Low color', value = 'white')
+                ),
+                column(
+                  width = 4,
+                  colourpicker::colourInput('bpHigh', 'High color', value = 'blue')
+                )
+              )
+            ),
+            hr(),
           )
         )
       ),
-      hr(),
-      withBusyIndicatorUI(actionButton("plotBubbleplot", "Plot Bubbleplot")),
-      div(
-        style = 'height:800px;',
-        plotOutput("Bubbleplot")
-      )
+      
     )
   )
 )
